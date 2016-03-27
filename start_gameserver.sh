77a672e81ee526bbe6da436ddf9e4c6809b12fb4
cinoss <tiencuong.dang@gmail.com>
2016-03-27 17:50:06 +0700

    strict game server with no master

--- start_gameserver.sh
+++ start_gameserver.sh
@@ -13,7 +13,7 @@ echo [BATCH] Starting Server without MasterServer
 while true
 do
 	clear
-	$node server/index.js
+	$node server/index.js --nomaster
 	echo -------------------------------------------------------
 	echo [BATCH] Server Shutdown, waiting 15 seconds before a restart.
 	sleep 15
